2cbd19bed9b1df7b18f4d5ec8b4bc2d5bdd4de0f
2531800823 <2531800823@qq.com>
2021-11-17 16:05:24 +0800

    注入到mixins.less、variables.less 到全局样式

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.join(__dirname + '/src/assets/style/mixins.less'),
+        path.join(__dirname + '/src/assets/style/variables.less')
+      ]
+    }
+  }
+}
+
+
+
+
